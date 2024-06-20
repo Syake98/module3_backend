63da7ce9f2c7f4a5207d4d3f9f7d6e9cbc909d5e
Syake98 <syake1998@mail.ru>
2024-06-20 21:56:15 +0300

    Доп.задание 2

--- notes.controller.js
+++ notes.controller.js
@@ -57,4 +57,5 @@ module.exports = {
   getNotes,
   removeNote,
   editNote,
+  printNotes,
 };
--- public/app.js
+++ public/app.js
@@ -1,17 +1,44 @@
 document.addEventListener("click", ({ target }) => {
   const id = target.dataset.id;
+  const liNote = target.closest("li");
+
   if (target.dataset.type === "remove") {
     remove(id).then(() => {
-      target.closest("li").remove();
+      liNote.remove();
     });
   } else if (target.dataset.type === "edit") {
-    const newTitle = prompt("Введите новое название");
-    newTitle &&
-      edit(id, newTitle).then(() => {
-        target.closest("li").querySelector("span").innerText = newTitle;
-      });
+    const noteTitle = liNote.querySelector(".note-title");
+    const noteInput = liNote.querySelector(".note-input");
+    const saveBtn = liNote.querySelector('button[data-type="save"]');
+    const cancelBtn = liNote.querySelector('button[data-type="cancel"]');
+    const removeBtn = liNote.querySelector('button[data-type="remove"]');
+
+    noteTitle.hidden = true;
+    noteInput.hidden = false;
+    target.hidden = true;
+    saveBtn.hidden = false;
+    cancelBtn.hidden = false;
+    removeBtn.hidden = true;
+  } else if (target.dataset.type === "save") {
+    const newTitle = liNote.querySelector(".note-input").value;
+
+    edit(id, newTitle).then(() => {
+      liNote.querySelector(".note-title").innerText = newTitle;
+      resetEditMode(liNote);
+    });
+  } else if (target.dataset.type === "cancel") {
+    resetEditMode(liNote);
   }
 });
+
+function resetEditMode(listItem) {
+  listItem.querySelector(".note-title").hidden = false;
+  listItem.querySelector(".note-input").hidden = true;
+  listItem.querySelector('button[data-type="save"]').hidden = true;
+  listItem.querySelector('button[data-type="cancel"]').hidden = true;
+  listItem.querySelector('button[data-type="edit"]').hidden = false;
+  listItem.querySelector('button[data-type="remove"]').hidden = false;
+}
 
 async function remove(id) {
   await fetch(`/${id}`, { method: "DELETE" });
